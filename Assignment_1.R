615dff64d05fb30b131b97a56b119b3fddc1640f
Jonathan Mostovoy <jmostovoy@gmail.com>
2017-01-26 19:24:57 -0500

    Added Preamble

--- Assignment_1.R
+++ Assignment_1.R
@@ -1,3 +1,28 @@
-setwd(~/Documents/Time_Series)
-dollar <- read.table(file="~/Downloads/dollar.txt", 
-                     header = FALSE)
+#Preamble:
+
+library(MASS)
+library(moments)
+library(car)
+library(zoo)
+library(lmtest)
+library(sandwich)
+library(strucchange)
+library(urca)
+library(vars)
+library(lmtest)
+library(vars)
+library(ggplot2)
+library(scales)
+library(gridExtra)
+library(lubridate)
+
+#Question 1
+
+setwd("~/Documents/Time_Series")
+dollar <- scan(file="dollar.txt")
+dollar <- ts(log(dollar))
+View(dollar)
+returns <- diff(dollar)
+View(returns)
+corrgram(t(dollar))
+
