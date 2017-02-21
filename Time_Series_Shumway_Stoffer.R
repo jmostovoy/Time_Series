19f49a538b493e8fbbfaa994e0fe79dc68cb78ef
Jonathan Mostovoy <jmostovoy@gmail.com>
2017-02-21 14:38:44 -0500

    small update

--- Time_Series_Shumway_Stoffer.R
+++ Time_Series_Shumway_Stoffer.R
@@ -25,6 +25,9 @@ mtext("Time (1 pt = 2 sec)", side=1, line=2)
 
 #pg 6
 
-
+w = rnorm(500,0,1) # 500 N(0,1) variates 
+v = filter(w, sides=2, rep(1/3,3)) # moving average par(mfrow=c(2,1))
+plot.ts(w, main="white noise")
+plot.ts(v, ylim=c(-3,3), main="moving average")
 
 
