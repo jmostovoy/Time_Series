cb6d116a42ddfa5767bed1c587064faaec45d4d4
Jonathan Mostovoy <jmostovoy@gmail.com>
2017-03-08 23:43:05 -0500

    Begun Assignment #3

--- Time_Series_Shumway_Stoffer.R
+++ Time_Series_Shumway_Stoffer.R
@@ -30,4 +30,12 @@ v = filter(w, sides=2, rep(1/3,3)) # moving average par(mfrow=c(2,1))
 plot.ts(w, main="white noise")
 plot.ts(v, ylim=c(-3,3), main="moving average")
 
+#pg 10
+
+cs = 2*cos((2*pi*1:500)/50 + .6*pi)
+w = rnorm(500,0,1)
+par(mfrow=c(3,1), mar=c(3,2,2,1), cex.main=1.5)
+plot.ts(cs, main=expression(2*cos(2*pi*t/50+.6*pi)))
+plot.ts(cs+w, main=expression(2*cos(2*pi*t/50+.6*pi) + N(0,1)))
+plot.ts(cs+5*w, main=expression(2*cos(2*pi*t/50+.6*pi) + N(0,25)))
 
